c66491e21ec95f8b7aee892a50aa74cdfe748e62
wehba <Lenovo@wehba.com>
2021-11-09 23:17:10 +0100

    add new file MYREDME how to use queck project

---
+++ src/main/java/webscraper/Tester.java
@@ -0,0 +1,70 @@
+package webscraper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Tester {
+
+    public static List<TagCounter> runSequental() {
+        List<TagCounter> urls = new ArrayList();
+        urls.add(new TagCounter("https://www.fck.dk"));
+        urls.add(new TagCounter("https://www.google.com"));
+        urls.add(new TagCounter("https://politiken.dk"));
+        urls.add(new TagCounter("https://cphbusiness.dk"));
+        for (TagCounter tc : urls) {
+            tc.doWork();
+        }
+        return urls;
+    }
+
+    public static List<TagCounter> runParrallel() throws Exception{
+        List<TagCounter> urls = new ArrayList();
+        urls.add(new TagCounter("https://www.fck.dk"));
+        urls.add(new TagCounter("https://www.google.com"));
+        urls.add(new TagCounter("https://politiken.dk"));
+        urls.add(new TagCounter("https://cphbusiness.dk"));
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        List<Future<TagCounter>> list = new ArrayList<>();
+        for (TagCounter tc : urls) {
+            Future<TagCounter> fut = executorService.submit(() -> {
+                tc.doWork();
+                return tc;
+            });
+            list.add(fut);
+        }
+        List<TagCounter> result = new ArrayList<>();
+        for (Future<TagCounter> fut : list) {
+            result.add(fut.get());
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        long timeSequental;
+        long start = System.nanoTime();
+
+        List<TagCounter> fetchedData = new Tester().runSequental();
+        long end = System.nanoTime();
+        timeSequental = end - start;
+        System.out.println("Time Sequential: " + ((timeSequental) / 1_000_000) + " ms.");
+
+        for (TagCounter tc : fetchedData) {
+            System.out.println("Title: " + tc.getTitle());
+            System.out.println("Div's: " + tc.getDivCount());
+            System.out.println("Body's: " + tc.getBodyCount());
+            System.out.println("----------------------------------");
+        }
+
+        /*
+        
+        start = System.nanoTime();
+        //TODO Add your parrallel calculation here     
+        long timeParallel = System.nanoTime() - start;
+        System.out.println("Time Parallel: " + ((timeParallel) / 1_000_000) + " ms.");
+        System.out.println("Paralle was " + timeSequental / timeParallel + " times faster");
+       
+         */
+    }
+}
